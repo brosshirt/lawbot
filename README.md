80552b4c6d176d9be4c01206dcb0ad8ee65de4aa
Benjamin Rosshirt <benjaminrosshirt@Benjamins-MacBook-Pro.local>
2024-03-27 11:45:05 -0400

    Basic starter code for embeddings and gpt-4

--- README.md
+++ README.md
@@ -4,5 +4,4 @@ Law students use the lawbot to consult their notes.
 
 ## Current task
 
-1. Set up basic python project
-2. Embed some text using the voyage-lite-02-instruct embedding algorithm
+1. Find solid vectorDB that's easy to work with, play around with it
--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,45 @@
-print("hello")
+import voyageai
+from openai import OpenAI
+
+import os
+from dotenv import load_dotenv
+load_dotenv() 
+
+# print(os.getenv("OPENAI_API_KEY"))
+
+# client = OpenAI(
+#     # This is the default and can be omitted
+#     api_key=os.environ.get("OPENAI_API_KEY")
+# )
+
+# chat_completion = client.chat.completions.create(
+#     messages=[
+#         {
+#             "role": "user",
+#             "content": "Tell me a story about a duck",
+#         }
+#     ],
+#     model="gpt-4-turbo-preview" # always points to latest gpt-4-turbo-preview model,
+# )
+
+# print(chat_completion.choices[0].message.content)
+
+
+
+
+vo = voyageai.Client()
+# This will automatically use the environment variable VOYAGE_API_KEY.
+# Alternatively, you can use vo = voyageai.Client(api_key="<your secret key>")
+
+texts = [
+    "The Mediterranean diet emphasizes fish, olive oil, ...",
+    "Photosynthesis in plants converts light energy into ...",
+    "20th-century innovations, from radios to smartphones ...",
+    "Rivers provide water, irrigation, and habitat for ...",
+    "Apple’s conference call to discuss fourth fiscal ...",
+    "Shakespeare's works, like 'Hamlet' and ...",
+]
+
+# Embed the documents
+result = vo.embed(texts, model="voyage-lite-02-instruct", input_type="document")
+print(result.embeddings)
