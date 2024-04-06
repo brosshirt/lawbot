4015c39546600c60b23fd81a57528da350ce00bb
Benjamin Rosshirt <benjaminrosshirt@benjamins-mbp.mynetworksettings.com>
2024-04-06 13:08:58 -0400

    Backend serves frontend

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 
 from lib.services import get_vo_client, get_openai_client, get_pinecone_index, get_articles, get_gpt_response
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 
 
@@ -8,10 +8,21 @@ vo_client = get_vo_client()
 openai_client = get_openai_client()
 pinecone_index = get_pinecone_index()
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend/build', static_url_path='')
 CORS(app)  # Enable CORS for all routes
 
 
+# Serve the appropriate webpage based on the react app
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:path>')
+def static_proxy(path):
+    # Serve the file (if exists) from the build folder
+    return send_from_directory(app.static_folder, path)
+
+# API
 @app.route('/', methods=['POST'])
 def home():
     # Get user input
